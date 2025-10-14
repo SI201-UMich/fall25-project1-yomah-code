cdbb76657f6cf73e07322362436c5c01c74d3b3e
Katia Hemphill <khemphill@Katias-MacBook-Air-4.local>
2025-10-14 14:08:09 -0400

    successfuly completed part 2 - CSV loading data

--- project1.py
+++ project1.py
@@ -1,7 +1,31 @@
-fname = input('Enter the file name: ')
-fhand = open(fname)
-with open("SampleSuperstore.csv") as inFile: 
-    lines = inFile.readlines() 
+# -----------------------------------------
+# SI 201 Project 1 
+# Your Name: Katia Hemphill
+# Your UMich Email: Katiah@umich.edu
+# Collaborators (if any)
+# -----------------------------------------
+
+import csv
+def read_csv_file(filename): #reads a CSV file and returns a list of dictionaries
+    with open(filename, newline='', encoding ='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+    return data 
+# --- MAIN PROGRAM ---
+def main():
+    filename = input("SampleSuperstore.csv")
+    try:
+        data = read_csv_file(filename)
+        print("File loaded successfully!")
+        print("Number of rows:", len(data))
+        print("Column names:", list(data[0].keys()))
+        print("Sample row:", data[0])
+    except FileNotFoundError:
+        print("File not found. Make sure the CSV is in this folder and name is correct.")
+
+# Run main
+if __name__ == "__main__":
+    main()
 
 
 #testing for git commits
